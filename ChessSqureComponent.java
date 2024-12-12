bcc7530e26558f4414353e886c16a5f1fd7fc2fe
Stanislav Krasilnikovgit config --global user.name Stanislav <krasstansvz@gmail.com>
2024-12-12 20:22:30 +0300

    Created main game logic in ChessGame class. Started making GUI

---
+++ ChessSqureComponent.java
@@ -0,0 +1,16 @@
+import javax.swing.*;
+
+public class ChessSqureComponent extends JButton {
+    private int row;
+    private int col;
+
+    public ChessSqureComponent(int row, int col) {
+        this.row = row;
+        this.col = col;
+        initButton();
+    }
+
+    private void initButton() {
+
+    }
+}
